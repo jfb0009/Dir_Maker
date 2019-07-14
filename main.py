51123f1c7478c82395a2266dcba600414f86a7e4
Jose Garcia Becerra <jfb0009@uah.edu>
2019-07-14 17:26:25 -0500

    Finished program Commented missing comments

--- main.py
+++ main.py
@@ -1,58 +1,37 @@
 # Import necessary libraries
 
-import os
+from create_folder import create_folder
 import csv
 
 
-def create_folder(path, name1, name2 = None):
-# Function to take in a path and the names of the folders it has to create
-
-    if name2 == None: # If it is just making the cities folder it runs this one
-        try: 
-            os.mkdir(os.path.join(path, name1))
-        except OSError:  
-            print ("Creation of the directory %s failed" % os.path.join(path, name1))
-        else:  
-            print ("Successfully created the directory %s " % os.path.join(path, name1))
-
-    else: # If it is making the cities folder it uses this one
-        try: 
-            os.mkdir(os.path.join(path, name1, name2))
-        except OSError:  
-            print ("Creation of the directory %s failed" % os.path.join(path, name1, name2))
-        else:  
-            print ("Successfully created the directory %s " % os.path.join(path, name1, name2))
-
-
-
 def main():
-    path = "C:\\Users\\Jose Garcia\\Desktop\\Total"     # The path for the cities folders to be made
-    cities = []                                         # The list for the cities names
-    addresses = []                                      # The list for the address folders already created
+    path = "C:\\Users\\Jose Garcia\\Desktop\\Total Fire Protection"     # The path for the cities folders to be made
+    cities = []                                                         # The list for the cities names
+    addresses = []                                                      # The list for the address folders already created
 
-
+    # Opens the csv and reads it into a list 
     with open("Competitor_Drawing_Sorted.csv") as file:     
-        fl = csv.reader(file)
+        fl = list(csv.reader(file))
 
-    
-        for line in fl:
-            if line[1] not in cities and line[1] != '': # Appends all of the cities to the cities list only once
-                cities.append(line[1])
+    # For every line it takes off any trailing spaces on the cities and appends them to the list only once
+    for line in fl:
+        if line[1].rstrip() not in cities and line[1] != '': # Appends all of the cities to the cities list only once
+            cities.append(line[1].rstrip())
 
-
-        unique_cities = set(cities)
-        for city in unique_cities:
-            city.strip()
-            create_folder(path, city)
+    # Makes a folder for each city
+    for city in cities:
+        create_folder(path, city)
         
-
-        """ for line in fl:
-            for city in cities:
-                if line[0] not in addresses and city == line[1] and line[0] != '':
-                    create_folder(path, city.strip(), line[0].strip())
+    # For every sublist it checks if the address has already been made if not it finds the city it goes in and makes a folder
+    # It then appends the address to the lsit of created folders
+    for line in fl:
+        for city in cities:
+            if (line[0] not in addresses) and (city == line[1].rstrip()) and (line[0].rstrip() != ''):
+                create_folder(path, city, line[0])
             
-            addresses.append(line[0]) """
+        addresses.append(line[0])
         
+
     return
 
 
